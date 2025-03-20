7efa4cbc76a7c03810ebef1dbd6a1ae2aa2321a3
Uchral <tengistengis093@gmail.com>
2025-03-20 15:49:13 +0800

    first

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './component/header'
+import Body from './component/body'
+
+function App() {
+
+  return (
+    <>
+      
+        <Header/>
+        <Body/>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/component/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './header.css';
+
+function Header() {
+  return (
+      <div className='nav'>
+        <nav>
+          <a href="">Home</a>
+          <a href="">About</a>
+          <a href="">Performance</a>
+          <a href="">Hobby</a>
+          <a href="">Contact</a>
+          <span></span>
+        </nav>
+      </div>
+  );
+}
+
+export default Header;
